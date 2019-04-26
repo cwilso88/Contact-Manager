a204c11f1a4d127b9c822a69fe775d95ff50bf76
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-04-26 18:32:47 -0400

    destructuring Header.js and Contact.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import Contact from './components/Contact';
+import Header from './components/Header';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Contact />
+        <Header branding="Contact Manager" />
+        <Contact 
+        name="Abby Cruz" 
+        email="avacruz@gmail.com" 
+        phone="555-404-7700"/>
+       
+        <Contact 
+        name="Adam Grahm" 
+        email="adamg@gmail.com" 
+        phone="555-343-2705"/>
       </div>
     );
   }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 
  class Contact extends Component {
   render() {
+    const { name, email, phone } = this.props;
     return (
       <div>
-        <h4>Mary Mac</h4>
+        <h4>{name}</h4>
         <ul>
-            <li>Email: marymac@gmila.com</li>
-            <li>Phone: 555-333-2222</li>
+            <li>Email: {email}</li>
+            <li>Phone: {phone}</li>
         </ul>
       </div>
     )
