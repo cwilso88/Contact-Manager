ccb333811ee058a76dcce38ea07fa1468dca38cf
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-04-26 23:22:29 -0400

    deleted Contact.js and replaced with Contacts.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import Contact from './components/Contact';
-import Header from './components/Header';
 import Contacts from './components/Contacts';
+import Header from './components/Header';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -12,15 +11,8 @@ class App extends Component {
       <div className="App">
         <Header branding="Contact Manager" />
         <div className="container">
-        <Contact 
-        name="Abby Cruz" 
-        email="avacruz@gmail.com" 
-        phone="555-404-7700"/>
-       
-        <Contact 
-        name="Adam Grahm" 
-        email="adamg@gmail.com" 
-        phone="555-343-2705"/>
+        
+        <Contacts />
         </div>
       </div>
     );
