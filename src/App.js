6b9349ddf3ce0c76bbff21b6e49190eb2e1535f7
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-04-26 22:58:46 -0400

    added bootrap styling to App.js and Contact.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Contact from './components/Contact';
 import Header from './components/Header';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
+        <div className="container">
         <Contact 
         name="Abby Cruz" 
         email="avacruz@gmail.com" 
@@ -18,6 +20,7 @@ class App extends Component {
         name="Adam Grahm" 
         email="adamg@gmail.com" 
         phone="555-343-2705"/>
+        </div>
       </div>
     );
   }
