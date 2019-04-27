b28b7209c16a9fed7f9c486da8b5617e2b8d1e4e
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-04-27 14:43:17 -0400

    the new delete function started

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,13 +11,19 @@ import PropTypes from 'prop-types';
         this.setState({ showContactInfo: !this.state.showContactInfo });
     };
 
+    onDeleteClick = e => {
+        console.log('clicked');
+    };
+
   render() {
     const { name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
 
     return (
       <div className="card card-body mb-3">
-        <h4>{name} <i onClick={this.onShowClick} className="fas fa-sort-down"></i> </h4>
+        <h4>{name} <i onClick={this.onShowClick} className="fas fa-sort-down" style={{ cursor: 'pointer'}}></i> 
+        <i className="fas fa-times" onClick={this.onDeleteClick} style={{ cursor: 'pointer', float: 'right', color: 'red'}}></i>
+        </h4>
         {showContactInfo ? (<ul className="list-group">
                 <li className="list-group-item">Email: {email}</li>
                 <li className="list-group-item">Phone: {phone}</li>
