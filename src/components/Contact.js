5e5f28a44bf13e39ff9c2344475998e54117b30c
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-04-27 14:19:43 -0400

    Passed down the state change to the UI with if/else statement to hide and show the contact info. Also changed the starting state to false to hide the contact deatails for a clean UI

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
  class Contact extends Component {
 
     state = {
-        showContactInfo: true
+        showContactInfo: false
     };
 
     onShowClick = e => {
@@ -13,13 +13,15 @@ import PropTypes from 'prop-types';
 
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
+
     return (
       <div className="card card-body mb-3">
         <h4>{name} <i onClick={this.onShowClick} className="fas fa-sort-down"></i> </h4>
-        <ul className="list-group">
-            <li className="list-group-item">Email: {email}</li>
-            <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        {showContactInfo ? (<ul className="list-group">
+                <li className="list-group-item">Email: {email}</li>
+                <li className="list-group-item">Phone: {phone}</li>
+                </ul>) : null}
       </div>
     )
   }
