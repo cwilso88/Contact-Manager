2f7e9ee289891410aef2092ecffb5db442540987
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-04-29 11:37:26 -0400

    completed form, add button, added value to each label field, and next I will add event handler to actually be able to type in each form area

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -7,6 +7,7 @@ class AddContact extends Component {
         phone: ''
     }
     render() {
+        const { name, email, phone } = this.state;
         return (
             <div className="card mb-3">
                 <div className="card-header">
@@ -15,8 +16,17 @@ class AddContact extends Component {
                         <form>
                             <div className="form-group">
                                 <label htmlFor="name">Name</label>
-                                <input type="text" name="name" className="form-control form-control-lg" placeholder="Enter Name Here..."/>
+                                <input type="text" name="name" value={name} className="form-control form-control-lg" placeholder="Enter Name Here..."/>
                             </div>
+                            <div className="form-group">
+                                <label htmlFor="email">Email</label>
+                                <input type="email" name="email" value={email} className="form-control form-control-lg" placeholder="Enter Email Here..."/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="phone">Phone</label>
+                                <input type="text" name="phone" value={phone} className="form-control form-control-lg" placeholder="Enter Phone Number..."/>
+                            </div>
+                            <input type="submit" value="Add Contact" className="btn btn-dark btn-block"/>
                         </form>
                     </div>
                 </div>
