79049c41add7a061843c8ace522be82dc4111fd0
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-01 10:05:38 -0400

    Brought in the consumer into AddContact.js and added all of the JSX into the consumer tags

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Consumer }  from '../../context'
 
 class AddContact extends Component {
     state = {
@@ -17,29 +18,37 @@ class AddContact extends Component {
 
     render() {
         const { name, email, phone } = this.state;
-        return (
-            <div className="card mb-3">
-                <div className="card-header">
-                    Add Contact
-                    <div className="card-body">
-                        <form onSubmit={this.onSubmit}>
-                            <div className="form-group">
-                                <label htmlFor="name">Name</label>
-                                <input onChange={this.onChange} type="text" name="name" value={name} className="form-control form-control-lg" placeholder="Enter Name Here..."/>
+
+        return(
+            <Consumer>
+                {value => {
+                    const { dispatch } = value;
+                    return(
+                        <div className="card mb-3">
+                            <div className="card-header">
+                                Add Contact
+                                <div className="card-body">
+                                    <form onSubmit={this.onSubmit}>
+                                        <div className="form-group">
+                                            <label htmlFor="name">Name</label>
+                                            <input onChange={this.onChange} type="text" name="name" value={name} className="form-control form-control-lg" placeholder="Enter Name Here..."/>
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="email">Email</label>
+                                            <input onChange={this.onChange} type="email" name="email" value={email} className="form-control form-control-lg" placeholder="Enter Email Here..."/>
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="phone">Phone</label>
+                                            <input onChange={this.onChange} type="text" name="phone" value={phone} className="form-control form-control-lg" placeholder="Enter Phone Number..."/>
+                                        </div>
+                                        <input type="submit" value="Add Contact" className="btn btn-dark btn-block"/>
+                                    </form>
+                                </div>
                             </div>
-                            <div className="form-group">
-                                <label htmlFor="email">Email</label>
-                                <input onChange={this.onChange} type="email" name="email" value={email} className="form-control form-control-lg" placeholder="Enter Email Here..."/>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="phone">Phone</label>
-                                <input onChange={this.onChange} type="text" name="phone" value={phone} className="form-control form-control-lg" placeholder="Enter Phone Number..."/>
-                            </div>
-                            <input type="submit" value="Add Contact" className="btn btn-dark btn-block"/>
-                        </form>
-                    </div>
-                </div>
-            </div>
+                        </div>
+                    )
+                }}
+            </Consumer>
         )
     }
 }
