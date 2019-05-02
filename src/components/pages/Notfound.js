8f19bfe6a95cdc10f4db243594012cd6ba1c8ba5
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-02 11:42:12 -0400

    done for now

--- src/components/pages/Notfound.js
+++ src/components/pages/Notfound.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default () => {
   return (
     <div className="display-4"> 
-      <h1>404 Page Not Found</h1>
+      <h1><span className="text-danger">404 Page Not Found</span></h1>
       <p className="lead">Sorry, that page does not exist.</p>
     </div>
   );
