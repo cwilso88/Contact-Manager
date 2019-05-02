2ce66493aac8b5f8e8b630e31c98343cc202721b
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-02 11:39:21 -0400

    page not found

---
+++ src/components/pages/Notfound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div className="display-4"> 
+      <h1>404 Page Not Found</h1>
+      <p className="lead">Sorry, that page does not exist.</p>
+    </div>
+  );
+};
